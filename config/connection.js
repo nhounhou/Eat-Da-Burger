fb5e197704dd4f1083609e3f11ec5bb5d77be7ac
Michel Nhouyvanisvong <nhounhou@hotmail.com>
2021-03-05 18:37:53 -0500

    attaching Heroku Database

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
+var connection;
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  // NOTE: Be sure to add your MySQL password here!
-  password: '12345678',
-  database: 'burgers_db',
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    // NOTE: Be sure to add your MySQL password here!
+    password: '12345678',
+    database: 'burgers_db',
+  });
+};
 
 // Make connection.
 connection.connect((err) => {
